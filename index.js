c7a77a3c3d093ca8fd942c2ee5ce0589b0763e07
Aleksandr Turchenko <alexandrt100@gmail.com>
2021-11-03 15:25:20 +0100

    Function calculator created(Fase 1 of exercice)

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const chalk = require("chalk");
 
-const numeros = process.argv.slice(2);
+const calculator = () => {
+  const numeros = process.argv.slice(2);
 
-const number1 = +process.argv.slice(2)[0];
-const number2 = +process.argv.slice(2)[1];
+  const number1 = +process.argv.slice(2)[0];
+  const number2 = +process.argv.slice(2)[1];
 
-if (typeof number1 === "number" && typeof number2 === "number") {
-  process.exit(1);
-}
+  if (isNaN(number1) || isNaN(number2)) {
+    console.log("Error, no era un numero!");
+    process.exit(1);
+  } else {
+    const summa = (number1, number2) => number1 + number2;
+    const rest = (number1, number2) => number1 - number2;
+    const multiplication = (number1, number2) => number1 * number2;
+    const division = (number1, number2) => number1 / number2;
 
-const summa = (number1, number2) => number1 + number2;
-const rest = (number1, number2) => number1 - number2;
-const multiplication = (number1, number2) => number1 * number2;
-const division = (number1, number2) => number1 / number2;
+    console.log("Resultados: ");
+    console.log(
+      `${number1} + ${number2} = ${chalk.red(summa(number1, number2))}`
+    );
+    console.log(
+      `${number1} - ${number2} = ${chalk.green(rest(number1, number2))}`
+    );
+    console.log(
+      `${number1} * ${number2} = ${chalk.blue(
+        multiplication(number1, number2)
+      )}`
+    );
+    console.log(
+      `${number1} / ${number2} = ${chalk.magenta(division(number1, number2))}`
+    );
+  }
+};
 
-console.log("Resultados: ");
-console.log(`${number1} + ${number2} = ${chalk.red(summa(number1, number2))}`);
-console.log(`${number1} - ${number2} = ${chalk.green(rest(number1, number2))}`);
-console.log(
-  `${number1} * ${number2} = ${chalk.blue(multiplication(number1, number2))}`
-);
-console.log(
-  `${number1} / ${number2} = ${chalk.magenta(division(number1, number2))}`
-);
-
-process.exit;
-console.log("process.exit: ", process.exit);
+calculator();
